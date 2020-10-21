23e5b6d3107443947a835861b841348d0a45c352
Muhammad Luthfi Farizqi Luthfi <luthfifarizqi23@gmail.com>
2020-10-21 02:22:47 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,19 @@
+//Operator overloading//
+
+let firstHalf = ["Luthfi f", "Rpl a"]
+let secondHalf = ["Jago", "IT"]
+let beatles = firstHalf + secondHalf
+print(beatles)
+
+
+/*
+let data =  "Sekolah IDN"
+let data1 = data +  " RPL"
+print(data1)
+*/
+
+/*
+let data = 42
+let doubleMeaning = 42 + 180
+print(doubleMeaning)
+*/
